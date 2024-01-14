4cd7844f1c66064b04c2db0a4a2b789622f5b739
Victor Rogers <victor.r.rogers@gmail.com>
2024-01-14 10:03:15 -0600

    finally a working navmesh

--- src/plugins/ship_builder.rs
+++ src/plugins/ship_builder.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_xpbd_3d::prelude::*;
+use oxidized_navigation::NavMeshAffector;
 
 const SHIP_LENGTH: i32 = 40;
 const SHIP_WIDTH: i32 = 10;
@@ -8,85 +9,86 @@ const SHIP_HEIGHT: i32 = 10;
 #[derive(Component)]
 struct Ship;
 
+#[derive(Component)]
+struct Deck;
+
 pub struct ShipBuilderPlugin;
 
 impl Plugin for ShipBuilderPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_ship);
+        app.add_systems(Startup, generate_ship);
     }
 }
 
-fn spawn_ship(
+fn generate_ship(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    // Thickness of the ship's walls, bottom, and top deck
+    let wall_thickness = 0.5;
+    // let half_length = SHIP_LENGTH as f32 / 2.0;
+    // let half_width = SHIP_WIDTH as f32 / 2.0;
+    // let height = SHIP_HEIGHT as f32 - wall_thickness; // Subtract top and bottom thickness
+
+    // Create the bottom of the ship
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Box::new(
+                SHIP_LENGTH as f32,
+                wall_thickness,
+                SHIP_WIDTH as f32,
+            ))),
+            material: materials.add(Color::hex("D18251").unwrap().into()),
+            transform: Transform::from_xyz(0.0, (-wall_thickness / 2.0) + 2.0, 0.0),
+            ..default()
+        },
+        Deck,
+        RigidBody::Static,
+        Collider::cuboid(SHIP_LENGTH as f32, wall_thickness, SHIP_WIDTH as f32),
+        NavMeshAffector,
+    ));
+
+    // Create the top deck of the ship
+    // commands.spawn((
+    //     PbrBundle {
+    //         mesh: meshes.add(Mesh::from(shape::Box::new(
+    //             SHIP_LENGTH as f32,
+    //             wall_thickness,
+    //             SHIP_WIDTH as f32,
+    //         ))),
+    //         material: materials.add(Color::hex("D18251").unwrap().into()),
+    //         transform: Transform::from_xyz(0.0, -wall_thickness / 2.0, 0.0),
+    //         ..default()
+    //     },
+    //     Transform::from_xyz(0.0, SHIP_HEIGHT as f32 - wall_thickness / 2.0, 0.0),
+    //     Collider::cuboid(
+    //         SHIP_LENGTH as f32 / 2.0,
+    //         wall_thickness / 2.0,
+    //         SHIP_WIDTH as f32 / 2.0,
+    //     ),
+    // ));
+
+    // Create the walls (port, starboard, bow, stern) of the ship
+    // Each wall is a separate entity
+
+    // Port
+    // commands.spawn((
+    // ));
+
+    // Starboard
+
+    // Bow (front)
+    // commands.spawn((Collider::cuboid(
+    //     half_length,
+    //     height / 2.0,
+    //     wall_thickness / 2.0,
+    // ),));
+
+    // Stern (back)
+    // commands.spawn((Collider::cuboid(
+    //     half_length,
+    //     height / 2.0,
+    //     wall_thickness / 2.0,
+    // ),));
 }
-
-fn generate_deck() {}
-
-fn generate_rails() {}
-
-// fn spawn_ship(
-//     mut commands: Commands,
-//     mut meshes: ResMut<Assets<Mesh>>,
-//     mut materials: ResMut<Assets<StandardMaterial>>,
-// ) {
-//     let cube_size = 1.0; // Size of each cube, adjust as needed
-//
-//     // Spawn the parent entity with a minimal Transform component
-//     commands
-//         .spawn((
-//             TransformBundle::default(),
-//             Ship,
-//             InheritedVisibility::default(),
-//         ))
-//         .with_children(|parent| {
-//             for x in 0..SHIP_LENGTH {
-//                 for z in 0..SHIP_WIDTH {
-//                     for y in 0..SHIP_HEIGHT {
-//                         // Check if the current position is on the boundary
-//                         if x == 0
-//                             || x == SHIP_LENGTH - 1
-//                             || z == 0
-//                             || z == SHIP_WIDTH - 1
-//                             || y == 0
-//                             || y == SHIP_HEIGHT - 1
-//                         {
-//                             parent.spawn((
-//                                 PbrBundle {
-//                                     mesh: meshes.add(Mesh::from(shape::Cube { size: cube_size })),
-//                                     material: materials.add(Color::hex("D18251").unwrap().into()),
-//                                     transform: Transform::from_xyz(x as f32, y as f32, z as f32),
-//                                     ..default()
-//                                 },
-//                                 RigidBody::Static,
-//                                 // DebugRender::default().with_collider_color(Color::YELLOW),
-//                                 Collider::cuboid(cube_size / 2.0, cube_size / 2.0, cube_size / 2.0),
-//                             ));
-//                         }
-//                     }
-//                 }
-//             }
-//             // Spawn railing
-//             for x in 0..SHIP_LENGTH {
-//                 for z in 0..SHIP_WIDTH {
-//                     let y = SHIP_HEIGHT; // One layer above the ship
-//                                          // Spawn railing on edges
-//                     if x == 0 || x == SHIP_LENGTH - 1 || z == 0 || z == SHIP_WIDTH - 1 {
-//                         parent.spawn((
-//                             PbrBundle {
-//                                 mesh: meshes.add(Mesh::from(shape::Cube { size: cube_size })),
-//                                 material: materials.add(Color::hex("D18251").unwrap().into()),
-//                                 transform: Transform::from_xyz(x as f32, y as f32, z as f32),
-//                                 ..default()
-//                             },
-//                             RigidBody::Static,
-//                             Collider::cuboid(cube_size / 2.0, cube_size / 2.0, cube_size / 2.0),
-//                             // DebugRender::default().with_collider_color(Color::YELLOW),
-//                         ));
-//                     }
-//                 }
-//             }
-//         });
-// }
