7830a8471e3ba6913a9017746d4025761caf83fa
Victor Rogers <victor.r.rogers@gmail.com>
2024-01-17 16:58:35 -0600

    implementing a fatigue/sleep system for the utility ai

--- src/components/sleep.rs
+++ src/components/sleep.rs
@@ -2,10 +2,9 @@ use bevy::prelude::*;
 use big_brain::prelude::*;
 
 #[derive(Clone, Component, Debug, ActionBuilder)]
-pub struct Sleep;
-// pub struct Sleep {
-//     /// The fatigue level at which the entity will stop sleeping.
-//     until: f32,
-//     /// The rate at which the fatigue level decreases while sleeping.
-//     per_second: f32,
-// }
+pub struct Sleep {
+    /// The fatigue level at which the entity will stop sleeping.
+    pub until: f32,
+    /// The rate at which the fatigue level decreases while sleeping.
+    pub per_second: f32,
+}
