92f4a6e49b25a89a113cf6b208043c4b18e0ebd8
Victor Rogers <victor.r.rogers@gmail.com>
2024-01-17 12:05:25 -0600

    begining to implement a sleeping action

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,5 +2,6 @@ mod camera;
 mod character;
 mod npc;
 mod player;
+mod sleep_area;
 
-pub use {camera::*, character::*, npc::*, player::*};
+pub use {camera::*, character::*, npc::*, player::*, sleep_area::*};
