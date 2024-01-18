eea974472e258f18a70288d3a78022bc3dd2dcb0
Victor Rogers <victor.r.rogers@gmail.com>
2024-01-18 08:26:44 -0600

    scorer for fatigue

--- src/components/fatigue_scorer.rs
+++ src/components/fatigue_scorer.rs
@@ -3,6 +3,5 @@ use big_brain::prelude::*;
 
 /// This component serves as a scorer for evaluating the entity's need to sleep based on its
 /// fatigue level.
-#[derive(Clone, Component, Debug, ScorerBuilder, Reflect, Default)]
-#[reflect(Component)]
+#[derive(Clone, Component, Debug, ScorerBuilder)]
 pub struct FatigueScorer;
