7b6360d1975850c4f90fb5084c3a797ce874b0f5
Victor Rogers <victor.r.rogers@gmail.com>
2024-01-21 14:30:51 -0600

    finding polygon path

--- src/components/navigation_path.rs
+++ src/components/navigation_path.rs
@@ -1,11 +1,13 @@
 use bevy::prelude::*;
 
+/// Component for storing a set of points to navigate along
 #[derive(Component, Debug, Clone)]
 pub struct NavigationPath {
     pub points: Vec<Vec3>,
 }
 
 impl Default for NavigationPath {
+    /// Sets up a NavigationPath with an empty path Vec
     fn default() -> Self {
         NavigationPath { points: Vec::new() }
     }
