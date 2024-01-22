d78c1dfdf8eb24ebf9299d576558ae5886dda5b8
Victor Rogers <victor.r.rogers@gmail.com>
2024-01-22 08:28:55 -0600

    working out conflicting queries in the move_to system

--- src/plugins/pathfinding.rs
+++ src/plugins/pathfinding.rs
@@ -1,5 +1,5 @@
 use crate::prelude::*;
-use bevy::prelude::*;
+use bevy::{prelude::*, reflect::Reflect};
 use big_brain::prelude::*;
 use oxidized_navigation::{
     query::{find_path, find_polygon_path, perform_string_pulling_on_path},
@@ -29,13 +29,12 @@ fn async_pathfinding_system(
 fn poll_pathfinding_tasks_system() {}
 
 fn move_to_nearest_system<T: Component + std::fmt::Debug + Clone>(
-    time: Res<Time>,
     nav_mesh: Res<NavMesh>,
     nav_mesh_settings: Res<NavMeshSettings>,
-    mut query: Query<&mut Transform, With<T>>,
-    mut thinkers: Query<&mut Transform, (With<HasThinker>, Without<T>)>,
-    mut path_query: Query<(&mut NavigationPath, &Transform), With<Actor>>,
     mut action_query: Query<(&Actor, &mut ActionState, &MoveToNearest<T>, &ActionSpan)>,
+    mut goal_query: Query<&mut Transform, (With<T>, Without<Actor>)>,
+    mut path_query: Query<(&mut NavigationPath, &Transform), With<Actor>>,
+    mut thinkers_query: Query<&mut Transform, (With<HasThinker>, Without<T>)>,
 ) {
     for (actor, mut action_state, move_to, span) in &mut action_query {
         let _guard = span.span().enter();
@@ -48,33 +47,35 @@ fn move_to_nearest_system<T: Component + std::fmt::Debug + Clone>(
             }
 
             ActionState::Executing => {
-                let (mut navigation_path, actor_transform) = path_query.get_mut(actor.0).unwrap();
-                // TODO: implement a function for goal_has_moved
-                let goal_has_moved = false;
+                let actor_transform = thinkers_query.get_mut(actor.0).unwrap();
+                for (mut navigation_path, _transform) in &mut path_query {
+                    // TODO: implement a function for goal_has_moved
+                    let goal_has_moved = false;
 
-                let goal_transform = query
-                    .iter()
-                    .map(|t| (t.translation, t))
-                    .min_by(|(a, _), (b, _)| {
-                        // We need partial_cmp here because f32 doesn't implement Ord.
-                        let delta_a = *a - actor_transform.translation;
-                        let delta_b = *b - actor_transform.translation;
-                        delta_a.length().partial_cmp(&delta_b.length()).unwrap()
-                    })
-                    .unwrap()
-                    .1;
+                    let goal_transform = goal_query
+                        .iter()
+                        .map(|t| (t.translation, t))
+                        .min_by(|(a, _), (b, _)| {
+                            // We need partial_cmp here because f32 doesn't implement Ord.
+                            let delta_a = *a - actor_transform.translation;
+                            let delta_b = *b - actor_transform.translation;
+                            delta_a.length().partial_cmp(&delta_b.length()).unwrap()
+                        })
+                        .unwrap()
+                        .1;
 
-                if navigation_path.points.is_empty() || goal_has_moved {
-                    if let Some(new_path) = calculate_path_blocking(
-                        &nav_mesh,
-                        &nav_mesh_settings,
-                        actor_transform.translation,
-                        goal_transform.translation,
-                    ) {
-                        navigation_path.points = new_path;
-                    } else {
-                        *action_state = ActionState::Failure;
-                        continue;
+                    if navigation_path.points.is_empty() || goal_has_moved {
+                        if let Some(new_path) = calculate_path_blocking(
+                            &nav_mesh,
+                            &nav_mesh_settings,
+                            actor_transform.translation,
+                            goal_transform.translation,
+                        ) {
+                            navigation_path.points = new_path;
+                        } else {
+                            *action_state = ActionState::Failure;
+                            continue;
+                        }
                     }
                 }
 
@@ -96,26 +97,28 @@ fn calculate_path_blocking(
     start: Vec3,
     goal: Vec3,
 ) -> Option<Vec<Vec3>> {
-    // Obtain a read lock on the navigation mesh and hold it in a variable
-    let nav_mesh_read_lock = nav_mesh.get().read().unwrap();
+    // Lock the nav_mesh for reading
+    let nav_mesh_lock = nav_mesh.get();
+    let nav_mesh = nav_mesh_lock.read().unwrap();
 
-    // Convert 'walkable_radius' from u16 to f32
-    let walkable_radius = nav_mesh_settings.walkable_radius.into();
-
+    // Find the polygon path using the navigation mesh
     let polygon_path = match find_polygon_path(
-        &nav_mesh_read_lock,
+        &nav_mesh,
         nav_mesh_settings,
         start,
         goal,
-        None, // Additional options, if needed
-        Some(&[walkable_radius, nav_mesh_settings.walkable_height.into()]), // Convert to f32
+        None, // You can specify options here if needed
+        Some(&[
+            nav_mesh_settings.walkable_radius.into(),
+            nav_mesh_settings.walkable_height.into(),
+        ]),
     ) {
         Ok(path) => path,
         Err(_) => return None,
     };
 
     // Perform string pulling on the polygon path to get a Vec3 path
-    match perform_string_pulling_on_path(&nav_mesh_read_lock, start, goal, &polygon_path) {
+    match perform_string_pulling_on_path(&nav_mesh, start, goal, &polygon_path) {
         Ok(string_path) => Some(string_path),
         Err(_) => None,
     }
