be31206b839c9be5cb51a73e38df4997f7c6d19d
Victor Rogers <victor.r.rogers@gmail.com>
2024-02-27 07:20:11 -0600

    created stern cabin area collider

--- src/plugins/physics.rs
+++ src/plugins/physics.rs
@@ -11,11 +11,22 @@ pub struct PhysicsPlugin;
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
         // TODO: Add xpbd in here
-        app.register_type::<ColliderMarker>()
+        app.register_type::<AreaMarker>()
+            .register_type::<AreaName>()
+            .register_type::<ColliderMarker>()
             .register_type::<NavMeshMarker>()
+            .add_systems(Update, read_area_markers.run_if(in_state(AppStates::Next)))
             .add_systems(Update, read_colliders.run_if(in_state(AppStates::Next)));
     }
 }
+
+#[derive(Debug, Clone, Eq, PartialEq, Component, Reflect, Serialize, Deserialize, Default)]
+#[reflect(Component, Serialize, Deserialize)]
+pub struct AreaMarker;
+
+#[derive(Debug, Clone, Eq, PartialEq, Component, Reflect, Serialize, Deserialize, Default)]
+#[reflect(Component, Serialize, Deserialize)]
+pub struct AreaName(String);
 
 #[derive(Debug, Clone, Eq, PartialEq, Component, Reflect, Serialize, Deserialize, Default)]
 #[reflect(Component, Serialize, Deserialize)]
@@ -24,6 +35,29 @@ pub struct ColliderMarker;
 #[derive(Debug, Clone, Eq, PartialEq, Component, Reflect, Serialize, Deserialize, Default)]
 #[reflect(Component, Serialize, Deserialize)]
 pub struct NavMeshMarker;
+
+// TODO: This can probably be combined with read_colliders()
+#[sysfail(log(level = "error"))]
+pub fn read_area_markers(
+    area_marker_query: Query<(Entity, &AreaMarker), Added<AreaMarker>>,
+    mut commands: Commands,
+    children: Query<&Children>,
+    meshes: Res<Assets<Mesh>>,
+    mesh_handles: Query<&Handle<Mesh>>,
+) -> Result<()> {
+    for (entity, _area_marker) in area_marker_query.iter() {
+        let mesh = find_mesh(entity, &children, &meshes, &mesh_handles)
+            .context("Failed to find mesh for area collider")?;
+        let collider = Collider::trimesh_from_mesh(mesh)
+            .context("Failed to create area collider from mesh")?;
+
+        println!("Inserting Sensor");
+        commands
+            .entity(entity)
+            .insert((collider, RigidBody::Static, Sensor, Visibility::Hidden));
+    }
+    Ok(())
+}
 
 #[sysfail(log(level = "error"))]
 pub fn read_colliders(
