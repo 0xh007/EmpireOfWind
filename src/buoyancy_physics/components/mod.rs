2dd2f0e412abe1d495efc23e2996aa44312110fc
Victor Rogers <victor.r.rogers@gmail.com>
2024-06-25 09:33:09 -0500

    buoyancy physics plugin in work

---
+++ src/buoyancy_physics/components/mod.rs
@@ -0,0 +1,5 @@
+mod buoyancy;
+mod buoyancy_marker;
+mod vec3i;
+mod voxel;
+mod voxel_visual;
--- src/mod.rs
+++ src/mod.rs
@@ -1,24 +1,3 @@
 mod area_management;
 mod empire_of_wind_plugins;
-
-// pub mod components;
-// pub mod constants;
-// pub mod events;
-// pub mod plugins;
-// pub mod resources;
-// pub mod systems;
-// pub mod utils;
-
-// pub mod prelude {
-//     pub use {
-//         components::*,
-//         constants::*,
-//         events::*,
-//         plugins::*,
-//         resources::*,
-//         systems::*,
-//         utils::*,
-//     };
-//
-//     use super::*;
-// }
+mod buoyancy_physics;
